9ff4f1680565270a3c73eba08087eb93e720c89e
jackowiak <matyskiewicz.j@gmail.com>
2019-01-14 22:47:04 +0100

    fix scoreboard styling

---
+++ .history/webapp/app/routes/scoreboard/scoreboard.styles_20190114224357.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import AppBar from '@material-ui/core/AppBar';
+import MuiToolbar from '@material-ui/core/Toolbar';
+import MoreVert from '@material-ui/icons/MoreVert';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+
+import { getColor, getUnit } from '../../utils/rendering';
+
+
+export const Bar = styled(AppBar)`
+  && {
+    box-shadow: none;
+    background-color: ${getColor(['common', 'white'])}
+    padding-top: ${getUnit(0.5)}px;
+  }
+`;
+
+export const Toolbar = styled(MuiToolbar)`
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Header = styled(Typography)`
+  padding-left: ${getUnit(2)}px;
+`;
+
+export const MoreIcon = styled(MoreVert)`
+`;
+
+export const Score = styled(ListItemSecondaryAction)`
+  && {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const DrinkIcon = styled(LocalDrinkIcon)`
+  && {
+    color: ${getColor(['secondary', 'dark'])};
+  }
+`;
+
+export const ListWrapper = styled(List)`
+  && {
+    margin-bottom: 50px;
+  }
+`
